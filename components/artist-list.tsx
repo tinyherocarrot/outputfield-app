3d1415dfe32cb6e12bffa481334ebfbb2b5e6325
Andrew Huang <andrw.huang@gmail.com>
2023-12-12 14:14:47 -0800

    closes outputfield/outputfield-app#21

--- components/artist-list.tsx
+++ components/artist-list.tsx
@@ -7,14 +7,22 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
 import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { useEffect, useMemo, useState } from 'react';
-import { ArtistsByGenre, toSortedByDate, toSortedByDistance, toSortedByGenre, toSortedByName } from '@/lib/utils';
+import { useCallback, useEffect, useMemo, useReducer, useState } from 'react';
+import { ArtistsByGenre, removeProperty, toSortedByDate, toSortedByDistance, toSortedByGenre, toSortedByName } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { useToast } from "@/components/ui/use-toast"
+import { CaretSortIcon, CopyIcon, MagicWandIcon, MinusCircledIcon } from '@radix-ui/react-icons';
 
 type SortOption = 'alphabetical' | 'date' | 'location' | 'genre'
 
@@ -22,6 +30,7 @@ export type Location = Record<"location" | "coordinates", string>
 export interface Artist {
   name: string;
   email: string;
+  url: string;
   genre: string;
   dateAdded: string;
   location__description: string;
@@ -29,10 +38,57 @@ export interface Artist {
   previewImg: string;
 }
 
+type Add = {
+  type: 'ADD',
+  key: string,
+  artist: Artist
+}
+
+type Delete = {
+  type: 'DELETE',
+  key: string,
+}
+
+type UploadWorksAction = Add | Delete
+
+interface UploadWorksState {
+  [key: string]: Artist;
+}
+
 export default function ArtistList({ data }: { data: Artist[] }) {
     const [sort, setSort] = useState<SortOption>('date')
     const [sortedData, setSortedData] = useState<Artist[] | ArtistsByGenre>(data)
+    const [isSelecting, setIsSelecting] = useState(false)
+    const { toast } = useToast()
 
+    const [state, dispatch] = useReducer(
+      (state: UploadWorksState, action: UploadWorksAction): UploadWorksState => {
+        const _state = { ...state }
+        switch (action.type) {
+        case 'ADD':
+          toast({ description: `Added ${action.artist.name}`})
+          return { ..._state, [action.key]: action.artist }
+        case 'DELETE':
+          return removeProperty(_state, `${action.key}`)
+        }
+      }, {}
+    );
+
+    const handleCopyArtists = useCallback(
+      async () => {
+        const text = Object.values(state).reduce(
+          (acc, curr) => acc.concat(`${curr.name} (${curr.genre})
+${curr.url}
+${curr.email}
+
+`)
+        , '')
+        await navigator.clipboard.writeText(text)
+        toast({ description: "Copied!" })
+      },
+      [toast, state],
+    )
+    
     useEffect(() => {
       let active = true
       if (data === undefined) setSortedData([]);
@@ -68,40 +124,20 @@ export default function ArtistList({ data }: { data: Artist[] }) {
     const content = useMemo(() => {
       if (sortedData.constructor === Array) {
         return [...sortedData as Artist[]].map((artist, i, arr) => (
-          <HoverCard key={artist.email}>
-            <HoverCardTrigger className="text-[75px]">
-              <span className="hover:underline">{artist.name}</span>
-              {(i + 1) !== arr.length ? ', ': '.'}
-            </HoverCardTrigger>
-            <HoverCardContent>
-              <Image
-                src={`/${artist.previewImg}`}
-                alt={`preview image of ${artist.name}'s website`}
-                width="200"
-                height="150"
-              />
-            </HoverCardContent>
-          </HoverCard>
+          <span key={artist.email} onClick={() => dispatch({ type: "ADD", key: artist.email, artist })}>
+            <ArtistName name={artist.name} previewImg={artist.previewImg} />
+            <span className='text-7xl'>{(i + 1) !== arr.length ? ', ': '.'}</span>
+          </ span>
         ))
       } else {
         return Object.entries(sortedData as ArtistsByGenre).map(([genre, artists]) => (
           <section key={genre} className='mb-14'>
             <h2>{genre}</h2>
             {artists.map((artist, i, arr) => (
-              <HoverCard key={artist.email}>
-                <HoverCardTrigger className="text-[75px]">
-                  <span className="hover:underline">{artist.name}</span>
-                  {(i + 1) !== arr.length ? ', ': '.'}
-                </HoverCardTrigger>
-                <HoverCardContent>
-                  <Image
-                    src={`/${artist.previewImg}`}
-                    alt={`preview image of ${artist.name}'s website`}
-                    width="200"
-                    height="150"
-                  />
-                </HoverCardContent>
-              </HoverCard>
+              <span key={artist.email} onClick={() => dispatch({ type: "ADD", key: artist.email, artist })}>
+                <ArtistName name={artist.name} previewImg={artist.previewImg} />
+                <span className='text-7xl'>{(i + 1) !== arr.length ? ', ': '.'}</span>
+              </span>
             ))}
           </section>
         ))
@@ -109,7 +145,7 @@ export default function ArtistList({ data }: { data: Artist[] }) {
     }, [sortedData])
 
     return (
-        <div className='w-full'>
+        <div className='w-full flex flex-col h-screen'>
           <Select onValueChange={(value) => setSort(value as SortOption)}>
             <SelectTrigger className="w-full mb-12">
               <SelectValue placeholder="Filter" />
@@ -121,7 +157,67 @@ export default function ArtistList({ data }: { data: Artist[] }) {
               <SelectItem value="genre">Genre</SelectItem>
             </SelectContent>
           </Select>
+          <div className="grow">
           {content}
+
+          </div>
+
+          {/* <Button size="icon" className='fixed right-10 top-1/2' onClick={() => setIsSelecting(!isSelecting)}>
+            <MagicWandIcon className="h-4 w-4" />
+          </Button> */}
+          
+          <Collapsible className='sticky bottom-0 z-30 bg-white py-4 border-t border-dotted'>
+            <div className="flex items-center justify-end space-x-4 px-4">
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" size="sm">
+                  Share Artists
+                  <CaretSortIcon className="h-4 w-4" />
+                  <span className="sr-only">Toggle</span>
+                </Button>
+              </CollapsibleTrigger>
+            </div>
+            <CollapsibleContent>
+              <div className="flex items-center justify-center space-x-4 px-4">
+                <Button variant="outline" size="icon" onClick={handleCopyArtists}>
+                  <CopyIcon className="h-4 w-4" />
+                </Button>
+              </div>
+              <div className="flex-col">
+                  {Object.values(state).map((artist) => (
+                    <div className="flex justify-between items-center w-full py-2" key={artist.email} >
+                      <div className='min-w-content'>
+                        <p className='italic'>{artist.name} ({artist.genre})</p>
+                        <p className='break-keep'>{artist.url}</p>
+                        <p>{artist.email}</p>
+                      </div>
+                      <div className="justify-self-end">
+                        <Button variant="outline" onClick={() => dispatch({ type: "DELETE", key: artist.email })}>
+                          <MinusCircledIcon className='h-4 w-4'/>
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+            </CollapsibleContent>
+          </Collapsible>
         </div>
     )
+}
+
+const ArtistName = ({ name, previewImg }: { name: string, previewImg: string }) => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger className="text-7xl cursor-copy">
+        <span className="hover:underline">{name}</span>
+      </HoverCardTrigger>
+      <HoverCardContent>
+        <Image
+          src={`/${previewImg}`}
+          alt={`preview image of ${name}'s website`}
+          width="200"
+          height="150"
+        />
+      </HoverCardContent>
+    </HoverCard>
+  )
 }
