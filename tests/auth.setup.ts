619843053bd023830cba706c64027dc1d87fc549
Andrew Huang <andrw.huang@gmail.com>
2024-02-02 12:39:43 -0800

    use new separated g service account creds

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,13 +4,24 @@ import { test as base } from '@playwright/test'
 
 import playwrightFirebasePlugin from '@nearform/playwright-firebase';
 
-const serviceAccount = JSON.parse(process.env.G_SERVICE_ACCOUNT!)
 const uid = process.env.NEXT_PUBLIC_FIREBASE_USER_UID as string;
+const serviceAccount = {
+    "type": process.env.G_SERVICE_ACCOUNT_TYPE!,
+    "project_id":process.env.G_SERVICE_ACCOUNT_PROJECT_ID!,
+    "private_key_id":process.env.G_SERVICE_ACCOUNT_PRIVATE_KEY_ID!,
+    "private_key": process.env.G_SERVICE_ACCOUNT_PRIVATE_KEY!,
+    "client_email":process.env.G_SERVICE_ACCOUNT_CLIENT_EMAIL!,
+    "client_id":process.env.G_SERVICE_ACCOUNT_CLIENT_ID!,
+    "auth_uri":process.env.G_SERVICE_ACCOUNT_AUTH_URI!,
+    "token_uri":process.env.G_SERVICE_ACCOUNT_TOKEN_URI!,
+    "auth_provider_x509_cert_url":process.env.G_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL!,
+    "client_x509_cert_url":process.env.G_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL!,
+    "universe_domain":process.env.G_SERVICE_ACCOUNT_UNIVERSE_DOMAIN!,
+}
 const options: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    // databaseURL: process.env
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    projectId: process.env.G_SERVICE_ACCOUNT_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
